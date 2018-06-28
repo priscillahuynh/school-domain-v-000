77688dec01859d60732dd05e661b18d926fc4e0b
Priscilla H <priscillamhuynh@gmail.com>
2018-06-28 13:33:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,10 @@ class School
   def add_student(student_name,grade)
     @roster[grade]=[student_name]
     #if roster has key of grade 9
-
+    if @roster[grade]=grade
     #then add student to grade
-
-    #if not, create new key
+    @roster[grade]<<student_name
+    #if not, create new key(grade)
 
     #then add student
   end
