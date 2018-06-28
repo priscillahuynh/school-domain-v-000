359fb58ae5a2aa027b13435b51a7ff1f7dbd2cd0
Priscilla H <priscillamhuynh@gmail.com>
2018-06-28 12:53:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,3 @@
 class School
-  def initialize(name,roster)
-    @name=name
-    @roster=roster
-  end
-
-  def name
-  @name
-  end
-
-  def roster
-    roster=[]
-  end
+  attr_accessor:name, :roster
 end
