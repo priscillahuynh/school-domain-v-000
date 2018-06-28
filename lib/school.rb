4d17836a8ce11588a8ae70c21e64cb4ec1620f65
Priscilla H <priscillamhuynh@gmail.com>
2018-06-28 13:10:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,10 +6,6 @@ class School
   end
 
   def add_student(student_name,grade)
-    if @roster[grade]
-       @roster[grade]<<student_name
-    else
-       @roster[grade]<<[student_name]
-    end
+    @roster={}
+    @roster[:grade]<<[]
   end
-end
