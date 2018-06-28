1acf1bdcda54125b675af4468332e9f2b6917457
Priscilla H <priscillamhuynh@gmail.com>
2018-06-28 13:40:40 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,12 +5,12 @@ class School
     @roster={}
   end
 
-  def add_student(student_name,grade)
-    if @roster[grade] == true
-       @roster[grade]<<student_name
-    else
-      @roster[grade] = []
-      @roster[grade]<<student_name
-    end
+  def add_student(student_name, grade)
+  if @roster[grade]
+    @roster[grade] << student_name
+  else
+    @roster[grade] = []
+    @roster[grade] << student_name
   end
+end
 end
